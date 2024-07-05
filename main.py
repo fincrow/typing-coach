d98206d92ec3d99deb1607c40c27355a64cd1f01
fincrow <fincrow@gmail.com>
2024-07-05 10:36:07 +0700

    Changed a comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from screens import StartScreen
 from tkinter import *
 
-# Display a tkinter box that displays a box of the words
+# Display a tkinter window
 window = Tk()
 window.title("Typing Test")
 window.config(width=200, height=200, padx=50, pady=50, bg="wheat1")
